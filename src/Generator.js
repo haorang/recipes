b343ae36fc33c5dabb30c94a74863bb1e5e158aa
haorang <ztop2525@gmail.com>
2022-04-27 21:56:39 -0700

    sdf

--- src/Generator.js
+++ src/Generator.js
@@ -43,12 +43,53 @@ function Generator(){
     '\n'+
     'Ingredients:\n';
 
+    const namePrompt = 'Name: Sugar Cookies\n'+
+    '\n'+
+    'Ingredients:\n'+
+    'flour, salt, baking soda, butter, sugar, egg, milk, vanilla extract\n'+
+    '\n'+
+    'Recipe:\n'+
+    '1. Whisk flour, salt, and baking soda together in a bowl. In a separate bowl, cream the butter, white sugar, and brown sugar together until mixture is light and fluffy, 3 to 4 minutes. Add the egg, milk, and vanilla extract. Whisk liquids together in small areas around the bowl, then all together to avoid separation.\n'+
+    '\n'+
+    '2. Pour dry ingredients into the wet ingredients; stir until flour is thoroughly mixed in. Stir in the chocolate chips.\n'+
+    '\n'+
+    '3. Transfer dough to a resealable plastic bag. Refrigerate until dough is firm, at least 2 hours.\n'+
+    '\n'+
+    '4. Preheat oven to 375 degrees F (190 degrees C). Line baking sheet with parchment paper.\n'+
+    '\n'+
+    '5. Scoop out rounded tablespoons of dough and place on prepared baking sheet, leaving 4 inches of space between cookies (about 8 per sheet). Bake in preheated oven until cookies are golden brown, about 12 minutes. Slide parchment and cookies onto a cooling rack for a few minutes. Remove parchment and finish cooling the cookies on the rack.\n'+
+    '\n'+
+    'Name: Shrimp Scampi\n'+
+    '\n'+
+    'Ingredients:\n'+
+    'butter, shrimp, olive oil, pepper, salt, shallots, linguine, red pepper flakes, garlic, shallots\n'+
+    '\n'+
+    'Recipe:\n'+
+    '1. Bring a large pot of salted water to a boil; cook linguine in boiling water until nearly tender, 6 to 8 minutes. Drain.\n'+
+    '\n'+
+    '2. Melt 2 tablespoons butter with 2 tablespoons olive oil in a large skillet over medium heat. Cook and stir shallots, garlic, and red pepper flakes in the hot butter and oil until shallots are translucent, 3 to 4 minutes. Season shrimp with kosher salt and black pepper; add to the skillet and cook until pink, stirring occasionally, 2 to 3 minutes. Remove shrimp from skillet and keep warm.\n'+
+    '\n'+
+    '3. Pour white wine and lemon juice into skillet and bring to a boil while scraping the browned bits of food off of the bottom of the skillet with a wooden spoon. Melt 2 tablespoons butter in skillet, stir 2 tablespoons olive oil into butter mixture, and bring to a simmer. Toss linguine, shrimp, and parsley in the butter mixture until coated; season with salt and black pepper. Drizzle with 1 teaspoon olive oil to serve.\n'+
+    '\n'+
+    'Name: Chicken Spaghetti\n'+
+    '\n'+
+    'Ingredients:\n'+
+    '  cooked chicken, spaghetti, cream of mushroom soup, cheddar cheese, green pepper, onion, pimentos, chicken broth, cayenne pepper, salt, pepper\n'+
+    '\n'+
+    'Recipe:\n'+
+    '1. Cook 1 cut up fryer and pick out the meat to make two cups. \n'+
+    '2. Cook spaghetti in same chicken broth until al dente. Do not overcook. When spaghetti is cooked, combine with remaining ingredients except additional 1 cup sharp cheddar.\n'+
+    '3. Place mixture in casserole pan and top with remaining sharp cheddar. Cover and freeze up to six months, cover and refrigerate up to two days, or bake immediately: 350 degrees for 45 minutes until bubbly. (If the cheese on top starts to get too cooked, cover with foil).\n'+
+    '\n'+
+    'Name:';
+
 
 
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false)
     const [ingredients, setIngredients] = useState("")
     const [apikey, setApikey] = useState("")
+    const [nameMode, setNameMode] = useState(false)
     const [recipeText, setRecipeText] = useState("Enter ingredients above (2-3 works best) and click generate!");
 
     const onIngredientsChange = e => {
@@ -59,6 +100,10 @@ function Generator(){
         setApikey(e.target.value)
     }
 
+    const onModeChange = e => {
+        setNameMode(!nameMode)
+    }
+
     const fetchRecipe = async () => {
         setLoading(true);
         setError(null);
@@ -66,7 +111,7 @@ function Generator(){
         try {
             // const tokens = ingredients.split(" ")
             // const commaSep = tokens.join(", ") + ","
-            const prompt = (ingredientPrompt + ingredients).trim() + ","
+            const prompt = nameMode? namePrompt + ingredients : (ingredientPrompt + ingredients).trim() + ","
             const configuration = new Configuration({
                 apiKey: apikey,
                 });
@@ -77,7 +122,8 @@ function Generator(){
                 });
             console.log(resp)
             console.log(resp.data.choices[0].text)
-            setRecipeText("Ingredients:\n"+ ingredients + "," + resp.data.choices[0].text);
+            const newRecipeText = nameMode? "Name: " + ingredients + resp.data.choices[0].text : "Ingredients:\n"+ ingredients + "," + resp.data.choices[0].text
+            setRecipeText(newRecipeText);
             setLoading(false);
         } catch (e) {
             setRecipeText("")
@@ -89,18 +135,22 @@ function Generator(){
     return (
       <div className=" transition bg-beige rounded-sm object-center content-center relative w-6/12
        border-solid border-2 border-transparent rounded flex justify-center items-center flex-col text-left" >
-           <div className="pl-2 mx-5 text-aqua ">Generate recipes below by entering ingredients seperated by a single space in the textbox below. Fill in your OpenAI API key and click generate! If you don't have a key click <a className="text-rat" target="_blank" href="https://beta.openai.com/account/api-keys">here</a> to sign up. </div>
+           <div className="pl-2 mx-5 text-aqua ">Generate recipes below by entering ingredients (or a dish name in name mode) seperated by a commas in the textbox below. Fill in your OpenAI API key and click generate! If you don't have a key click <a className="text-rat" target="_blank" href="https://beta.openai.com/account/api-keys">here</a> to sign up. </div>
            <div className="flex flex-row self-start mx-5">
-            <div className="p-2 mr-2 text-aqua">Ingredients to use:</div>
+            <div className="p-2 mr-2 text-aqua">{nameMode? "Name of dish:" : "Ingredients to use"}</div>
             <input
             className="p-1 border-solid border-2 border-aqua rounded"
             size="27"
             name="ingredients"
             onChange={onIngredientsChange}
             value={ingredients} 
-            placeholder={"carrots potatoes"}
+            placeholder={nameMode? "Peking-style Alligator" : "carrots, potatoes"}
             />
+             <button className = " mx-2 bg-aqua hover:bg-hover text-beige font-bold px-4 border-b-4 border-darker hover:border-aqua rounded" onClick={onModeChange}>
+                {nameMode? "Ingredient Mode": "Name Mode"}
+            </button>
            </div>
+
            <div className="flex flex-row mx-5 my-1 self-start">
             <div className="p-2 mr-2 text-aqua">OpenAI API Key:</div>
             <input
